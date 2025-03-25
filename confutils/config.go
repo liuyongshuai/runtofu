f740e723c05b049cbc4ab459fd1ecda57a85a3cd
liuyongshuai <liuyongshuai@hotmail.com>
2025-03-25 15:58:47 +0800

    update

---
+++ confutils/config.go
@@ -0,0 +1,5 @@
+// @author      Liu Yongshuai<liuyongshuai@hotmail.com>
+// @file        config.go
+// @date        2025-03-25 15:48
+
+package confutils
---
+++ confutils/tplfunc.go
@@ -0,0 +1,5 @@
+// @author      Liu Yongshuai<liuyongshuai@hotmail.com>
+// @file        tplfunc.go
+// @date        2025-03-25 15:48
+
+package confutils
