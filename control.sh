0b41a56c941fe8b3ca1a57535a502429b4f0f0ea
liuyongshuai <liuyongshuai@hotmail.com>
2025-03-26 20:38:57 +0800

    update

--- control.sh
+++ control.sh
@@ -4,35 +4,38 @@ workspace=$(cd $(dirname $0) && pwd -P)
 
 action="$1"
 conf="./conf/service.conf"
-mkdir log
+mkdir log > /dev/null 2>&1 &
 
 function usage() {
-        echo "manage runtofu process"
-        echo
-        echo "usage: $0 start|stop|restart|status"
-        echo
-        echo "    conf   optional, default ./conf/service.conf"
-        echo
+  echo "manage runtofu process"
+  echo
+  echo "usage: $0 start|stop|restart|status"
+  echo
+  echo "    conf   optional, default ./conf/service.conf"
+  echo
 }
 
 function start() {
-		nohup bin/runtofu -config=./conf/service.conf > ./abc.txt 2>&1 &
-		status
+  pid=$(pidof runtofu)
+  if [ "${pid}" == "" ]; then
+    nohup bin/runtofu -config=./conf/service.conf > ./abc.txt 2>&1 &
+    status
+  fi
 }
 
 function stop() {
 	pkill -f 'bin/runtofu -config=./conf/service.conf'
 	kill -9 $(ps aux|grep runtofu|grep -v grep|awk '{print $2}')
-    sleep 0.5
+  sleep 0.5
 }
 
 function status() {
-	pid=$(pgrep -f 'bin/runtofu -config=./conf/service.conf')
-        if [ "x$pid" == "x" ]; then
-            echo "runtofu is not running"
-        else
-            echo "runtofu is running with pid [$pid]"
-        fi
+  pid=$(pgrep -f 'bin/runtofu -config=./conf/service.conf')
+  if [ "x$pid" == "x" ]; then
+    echo "runtofu is not running"
+  else
+    echo "runtofu is running with pid [$pid]"
+  fi
 }
 
 case $action in
