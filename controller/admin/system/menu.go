17fe7f33c93bfbec799c567b8b59b7e6b742a51e
liuyongshuai <liuyongshuai@hotmail.com>
2025-03-31 19:14:56 +0800

    reformat code

--- controller/admin/system/menu.go
+++ controller/admin/system/menu.go
@@ -5,12 +5,12 @@
  */
 package system
 
-//system层的基类
+// system层的基类
 type AdminSystemMenuController struct {
 	AdminSystemBaseController
 }
 
-//开始
+// 开始
 func (c *AdminSystemMenuController) Run() {
 	c.TplName = "menu.tpl"
 	c.RenderHtml()
